43801a01e984350d39fe9abbc319d00a0ef245b9
github username <github email address>
2019-06-07 20:34:37 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   #get 'hello_world', to: 'static#hello_world'
   #get '/hello_world', to: 'hw#hello_world'
 
-  
-  get '/hello_world' to: 'static#hello_world'
+
+    get '/hello_world', to: 'static#hello_world'
 end
